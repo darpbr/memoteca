d917507d6a746d88ad1f2e1b4ceb400064ae7575
Diego de Amorim Rocha Peixoto <darpbr@gmail.com>
2025-11-16 22:41:27 -0300

    add editar cards

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento-model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PensamentoService } from '../pensamento';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -8,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class EditarPensamento {
 
+  pensamento: Pensamento = {
+    id: '',
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.service.buscarPorId(id).subscribe((pensamento) => {
+        this.pensamento = pensamento;
+      });
+    }
+  }
+
+  editarPensamento(): void {
+    this.service.editar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
+  }
+
+  cancelar(): void {
+    this.router.navigate(['/listarPensamento']);
+  }
 }
